4e87669509421d192c9052b35d2fe7780b7bd53f
Andreas Wingen <wingen@fusion.gat.com>
2019-03-26 14:18:32 -0700

    more features added

--- python/restartMafot.py
+++ python/restartMafot.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python2.7
 import os
 
-def main(path = './', logsPath = None):
+def findIncomplete(path = './', logsPath = None):
 	if not path[-1] == '/': path += '/'
 	if logsPath is None: 
 		logsPath = path
 	else: 
 		if not logsPath[-1] == '/': logsPath += '/'
-		
-	fileList = [f for f in os.listdir(logsPath) if '_Master.dat' in f]
+	
+	allFiles = os.listdir(logsPath)
+	fileList = [f for f in allFiles if '_Master.dat' in f]
+	inputList = [f for f in allFiles if (('_inner_' in f) | ('_outer_' in f) | ('_shelf_' in f))]
 	incompleteRuns = []
 	for file in fileList:
 		with open(logsPath + file) as f: lines = f.readlines()
@@ -16,8 +18,8 @@ def main(path = './', logsPath = None):
 		else: incompleteRuns.append(file)
 		
 	#print 'Runs:', incompleteRuns
-	
-	runNums = []
+	runNums = findMissing(fileList, inputList)	
+	#runNums = []
 	for file in incompleteRuns:
 		f = file.replace('_Master.dat','')
 		idx = f[::-1].find('_')
@@ -26,12 +28,12 @@ def main(path = './', logsPath = None):
 	
 	if len(runNums) == 0:
 		print 'All runs are complete. Nothing to do here.'
-		return
+		return []
 		
 	runNums.sort()
 	runList = ','.join([str(item) for item in runNums])
 	#print 'Nums:', runNums
-	print 'Incomplete runs:', runList
+	print 'List of runs incomplete:', runList
 	
 	if os.path.isfile(path + 'mafot.sbatch'):
 		with open(path + 'mafot.sbatch') as f: lines = f.readlines()
@@ -39,9 +41,146 @@ def main(path = './', logsPath = None):
 			for line in lines:
 				if '#SBATCH --array' in line: f.write('#SBATCH --array=' + runList + '\n')
 				else: f.write(line)
-	else: print 'No job Queue file found'
+	else: 
+		print 'No job Queue file found'
+		return []
+	
+	return runNums
+	
+	
+def findMissing(fileList, inputList):
+	runNums = []
+	for file in fileList:
+		f = file.replace('_Master.dat','')
+		idx = f[::-1].find('_')
+		n = int(f[-idx::])
+		runNums.append(n)
+	
+	N = 0
+	for file in inputList:
+		f = file.replace('.dat','')
+		idx = f[::-1].find('_')
+		n = int(f[-idx::])
+		if n > N: N = n
+				
+	N += 1
+	#print N, 'runs found'
+	
+	missing = []
+	for i in xrange(N):
+		if i not in runNums: missing.append(i)
+	
+	#if len(missing) > 0: print 'List of runs missing:', ','.join([str(item) for item in missing])
+	return missing
+	
+
+def keepWorking(path = './', logsPath = None):
+	from subprocess import call,check_output
+	import getpass,time
+	user = getpass.getuser()
+	if not path[-1] == '/': path += '/'	
+	
+	# wait until no more of your jobs in 'preemptable' queue
+	print 'Waiting for jobs to finish...'
+	bla = check_output(['squeue','-u',user])
+	while('preemptab' in bla):
+		time.sleep(600)		# wait 10 minutes; no CPU usage
+		bla = check_output(['squeue','-u',user])
+	
+	runNums = findIncomplete(path, logsPath)
+	while(len(runNums) > 0):
+	
+		# resubmit jobs
+		call(['sbatch', path + 'mafotRerun.sbatch'])
+		time.sleep(60)	# wait 60 sec
+	
+		# wait until no more of your jobs in 'preemptable' queue
+		print 'Waiting for jobs to finish...'
+		bla = check_output(['squeue','-u',user])
+		while('preemptab' in bla):
+			time.sleep(600)		# wait 10 minutes; no CPU usage
+			bla = check_output(['squeue','-u',user])
+		
+		# check new incomplete list
+		runNums = findIncomplete(path, logsPath)
 	
 	return
+	
+	
+def restartJob(batchfile, jobnumber = None, logsPath = None):
+	from subprocess import call,check_output
+	import getpass,time
+	user = getpass.getuser()
+
+	idx = batchfile[::-1].find('/')	# returns location of last '/' in batchfile or -1 if not found
+	if(idx == -1): path = './'
+	else: 
+		path = batchfile[0:-idx]	# path with a final '/'
+		batchfile = batchfile[-idx::]
+
+	#if not path[-1] == '/': path += '/'
+	if logsPath is None: 
+		logsPath = path
+	else: 
+		if not logsPath[-1] == '/': logsPath += '/'
+		
+	# read batchfile and get mpirun call
+	if os.path.isfile(path + batchfile):
+		with open(path + batchfile) as f: lines = f.readlines()
+	else: 
+		print 'Job file not found'
+		return
+		
+	for line in lines[::-1]:
+		if 'mpirun' in line: job = line.strip().split()
+	
+	# get mafot tool
+	if 'laminar' in job[3]: task = 'lam'
+	if 'foot' in job[3]: task = 'foot'
+	if 'plot' in job[3]: task = 'plot'
+	
+	# get file tag
+	chk = [True for item in job]
+	for i in xrange(4): chk[i] = False
+	for i,item in enumerate(job):
+		if (item[0] == '_'): chk[i] = False
+		if (item[0] == '-'): 
+			chk[i] = False
+			chk[i+1] = False
+	
+	tag = [job[i] for i in xrange(len(job)) if chk[i]][0]
+
+	print 'Waiting for job to finish...'
+	bla = check_output(['squeue','-u',user])
+	if jobnumber is None: jobnumber = 'preemptab' # use queue keyword instead
+	while(jobnumber in bla):
+		time.sleep(60)		# wait 1 minute; no CPU usage
+		bla = check_output(['squeue','-u',user])
+	
+	while(1):
+		fileList = [f for f in os.listdir(logsPath) if(('_Master.dat' in f) & (tag in f) & (task in f))]
+		incompleteRuns = []
+		for file in fileList:
+			with open(logsPath + file) as f: lines = f.readlines()
+			if 'Program terminates normally' in lines[-1]: continue
+			else: incompleteRuns.append(file)
+			
+		if len(incompleteRuns) == 0:
+			print 'Run complete.'
+			break
+		else: 
+			print 'Restarting: ', incompleteRuns[0].replace('log_','').replace('_Master.dat','')
+		
+		bla = check_output(['sbatch', path + batchfile])
+		jobnumber = bla.strip().split()[3]
+		print 'Submitted batch job', jobnumber
+		time.sleep(10)	# wait 10 sec for job to start
+
+		print 'Waiting for job to finish...'
+		bla = check_output(['squeue','-u',user])
+		while(jobnumber in bla):
+			time.sleep(60)		# wait 1 minute; no CPU usage
+			bla = check_output(['squeue','-u',user])
 	
 	
 if __name__ == '__main__':
@@ -50,10 +189,21 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description = 'Find and restart incomplete MAFOT runs', 
                 formatter_class = argparse.RawDescriptionHelpFormatter,
                 epilog = textwrap.dedent('''\
-                Examples: restartMafot.py'''))
+                Examples: restartMafot.py
+                          restartMafot.py -p'''))
 	
 	parser.add_argument('-d', '--dir', help = "set working dir", type = str, default = './')
+	parser.add_argument('-i', '--id', help = 'keep resubmitting job with initial number ID until complete, single run only! Use -j to set JOB file', type = str, default = None)
+	parser.add_argument('-j', '--job', help = 'keep resubmitting jobfile JOB until complete, default is mafot.sbatch, single run only!', type = str, default = None)
 	parser.add_argument('-l', '--logs', help = "set log dir, if different from working dir", type = str, default = None)
+	parser.add_argument('-p', '--persist', help = 'keep resubmitting until all jobs complete, job arrays only', action = 'store_true', default = False)
 	args = parser.parse_args()
 	
-	main(args.dir, args.logs)
+	if (args.job is not None) | (args.id is not None):
+		if args.job is None: job = 'mafot.sbatch'
+		else: job = args.job
+		restartJob(job, args.id, args.logs)
+	elif args.persist:
+		keepWorking(args.dir, args.logs)
+	else:
+		_ = findIncomplete(args.dir, args.logs)
